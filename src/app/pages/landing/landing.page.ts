4a2d4bac883c42efc4669f56c4c906d9eee3cb30
Conrad Bekondo <conradbekondo@gmail.com>
2026-05-08 09:43:14 +0100

    feat: implement landing page layout and theme service, update index and styles

--- src/app/pages/landing/landing.page.ts
+++ src/app/pages/landing/landing.page.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { isUserSignedIn } from '@app/features/auth/utils';
+import { ThemeService } from '@app/features/themeing/theme.service';
+import { NgxFlickeringGridComponent } from '@omnedia/ngx-flickering-grid';
+import { NgxGradientTextComponent } from '@omnedia/ngx-gradient-text';
+import { HlmButton } from '@spartan-ng/helm/button';
 
 @Component({
 	selector: 'dm-landing',
-	imports: [],
+	imports: [
+		RouterLink,
+		HlmButton,
+		NgxFlickeringGridComponent,
+		NgxGradientTextComponent
+	],
 	templateUrl: './landing.page.html',
 	styleUrl: './landing.page.scss',
 })
-export class LandingPage {}
+export class LandingPage {
+	protected signedIn = isUserSignedIn();
+	protected readonly theme = inject(ThemeService).themeSignal;
+}
