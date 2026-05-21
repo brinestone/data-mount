8499f9ab8683bc5b8862c6d010ba0944519cf5e3
Conrad Bekondo <conradbekondo@gmail.com>
2026-05-21 09:55:35 +0100

    feat(dashboard): recreate dashboard page with new structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ export const appRoutes: Routes = [
 	{
 		path: 'dashboard',
 		title: $localize`Dashboard`,
-		loadComponent: () => import('./features/dashboard/dashboard.page').then(m => m.DashboardPage),
+		loadComponent: () => import('./pages/dashboard/dashboard.page').then(m => m.DashboardPage),
 	},
 	{ path: '', pathMatch: 'full', redirectTo: 'dashboard' }
 ]
